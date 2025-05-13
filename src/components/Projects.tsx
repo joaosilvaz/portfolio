8ea999951c34c3b7c9d387107cc7d0eedb297806
joaosilvaz <joaovitoraz31@gmail.com>
2025-05-13 12:57:49 -0300

    feat: add projects component and install lib vanilla-tilt

---
+++ src/components/Projects.tsx
@@ -0,0 +1,157 @@
+import { Airplay } from 'lucide-react';
+import { motion } from 'framer-motion';
+import { useTranslations } from 'next-intl';
+import Link from "next/link";
+import { useState } from 'react';
+import { ChevronDown, ChevronUp } from 'lucide-react';
+import Image from "next/image";
+
+export default function Projects() {
+    const t = useTranslations('projects');
+    const [showAll, setShowAll] = useState(false);
+    const projects = [
+        {
+            title: t('titleProject1'),
+            description: t('descriptionProject1'),
+            image: "/images/ecosmart.png",
+            techs: ["/nextjs.svg", "/typescript.svg", "/tailwind.svg", "/react.svg"],
+            liveLink: "https://global-solution-s2.vercel.app/",
+            githubLink: "https://github.com/joaosilvaz/Global-Solution-S2",
+        },
+        {
+            title: t('titleProject2'),
+            description: t('descriptionProject2'),
+            image: "/images/god-of-war.png",
+            techs: ["/vite.svg", "/typescript.svg", "/tailwind.svg", "/react.svg"],
+            liveLink: "https://lp-check-point1.vercel.app/",
+            githubLink: "https://github.com/joaosilvaz/LP-CheckPoint1",
+        },
+        {
+            title: t('titleProject3'),
+            description: t('descriptionProject3'),
+            image: "/images/ocean-guard.png",
+            techs: ["/javascript.svg", "/html.svg", "/css.svg"],
+            liveLink: "https://ocean-guard-flax.vercel.app/",
+            githubLink: "https://github.com/joaosilvaz/OceanGuard",
+        },
+        {
+            title: t('titleProject4'),
+            description: t('descriptionProject4'),
+            image: "/images/e-commerce.png",
+            techs: ["/html.svg", "/css.svg", "/javascript.svg"],
+            liveLink: "https://e-commerce-two-puce.vercel.app/",
+            githubLink: "https://github.com/joaosilvaz/e-commerce",
+        },
+        {
+            title: t('titleProject5'),
+            description: t('descriptionProject5'),
+            image: "/images/bank-project.jpg",
+            techs: ["/java.svg", "/spring-boot.svg"],
+            liveLink: "",
+            githubLink: "https://github.com/joaosilvaz/bank-project",
+        },
+    ];
+    const visibleProjects = showAll ? projects : projects.slice(0, 3);
+
+    return (
+        <section id="projetos" className="bg-white dark:bg-[var(--bg-gradient)] text-black dark:text-white pt-50 pb-20 px-6 md:px-16">
+            <div className="max-w-7xl mx-auto md:text-left text-center">
+                <motion.h2
+                    className="text-4xl font-bold mb-4"
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: false, amount: 0.2 }}
+                    transition={{ duration: 0.8 }}
+                >
+                    {t('title')}
+                </motion.h2>
+                <p className="text-black dark:text-gray-300 mb-12 max-w-xl">
+                    {t('description')}
+                </p>
+
+                <div className="grid gap-10 grid-cols-1 min-[900px]:grid-cols-2 min-[1218px]:grid-cols-3">
+                    {visibleProjects.map((project, index) => (
+                        <motion.div
+                            key={index}
+                            initial={{ opacity: 0, y: 50 }}
+                            whileInView={{ opacity: 1, y: 0 }}
+                            viewport={{ once: false, amount: 0.2 }}
+                            transition={{ duration: 0.8 }}
+                            className="h-auto border border-cyan-400 rounded-xl px-8 py-8 bg-white dark:bg-[var(--bg-gradient)] shadow-md flex flex-col justify-between"
+                        >
+                            <div className="relative">
+                                <Image
+                                    src={project.image}
+                                    alt={project.title}
+                                    className="w-full h-48 object-cover rounded-md hover:opacity-50 cursor-pointer transition-all duration-200"
+                                    width={500}
+                                    height={300}
+                                />
+                                <a
+                                    href={project.liveLink}
+                                    target="_blank"
+                                    className="absolute inset-0 bg-black bg-opacity-40 flex justify-center items-center opacity-0 hover:opacity-60 transition-opacity duration-300"
+                                >
+                                    <Airplay className="text-white" size={32} />
+                                </a>
+                            </div>
+                            <h3 className="md:text-2xl text-xl font-bold mb-4 mt-4">{project.title}</h3>
+                            <p className="text-[rgb(146,158,176)] mb-6 md:text-sm text-sm">{project.description}</p>
+
+                            <div className="flex flex-wrap gap-4 mb-4 md:justify-start justify-center">
+                                {project.techs.map((tech, idx) => (
+                                    <Image
+                                        key={idx}
+                                        src={tech}
+                                        alt={`Tecnologia ${idx}`}
+                                        className="w-7 h-7 cursor-pointer hover:scale-115 transition-all duration-300"
+                                        width={28}
+                                        height={28}
+                                    />
+                                ))}
+                            </div>
+
+                            <div className="flex flex-col min-[370px]:flex-row justify-between gap-3 mt-auto pt-3">
+                                {project.liveLink && (
+                                    <Link
+                                        href={project.liveLink}
+                                        target="_blank"
+                                        className="bg-gradient-to-l from-purple-500 to-cyan-400 text-white px-3 py-3 md:px-3 rounded-full font-semibold text-sm transition-all ease-in duration-100 dark:hover:shadow-[0px_4px_15px_rgba(255,255,255,0.4)] text-center w-full min-[370px]:w-auto"
+                                    >
+                                        {t('deploy')}
+                                    </Link>
+                                )}
+                                <Link
+                                    href={project.githubLink}
+                                    target="_blank"
+                                    className="border px-2 py-3 rounded-full font-semibold md:px-4 lg:px-3 text-sm text-black dark:text-white border-black dark:border-white
+                                    hover:text-white hover:bg-black dark:hover:bg-white dark:hover:text-black
+                                    transition-all ease-in duration-200 text-center w-full min-[370px]:w-auto"
+                                >
+                                    {t('github')}
+                                </Link>
+                            </div>
+                        </motion.div>
+                    ))}
+                </div>
+
+                <div className="flex justify-center mt-10">
+                    <button
+                        onClick={() => setShowAll(!showAll)}
+                        className="bg-gradient-to-r from-purple-500 to-blue-500 shadow-lg hover:opacity-90 text-white px-5 py-2 rounded-lg font-semibold transition duration-200 cursor-pointer"
+                    >
+                        {showAll ? (
+                            <>
+                                {t('seeLess')} <ChevronUp className="inline-block ml-2" size={16} />
+                            </>
+                        ) : (
+                            <>
+                                {t('seeMore')} <ChevronDown className="inline-block ml-2" size={16} />
+                            </>
+                        )}
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
+}
---
+++ src/components/Tecnologies.tsx
@@ -0,0 +1,160 @@
+import Image from 'next/image'; 
+import { motion } from 'framer-motion';
+import TiltCard from '@/components/TiltCard';
+import { useTranslations } from 'next-intl';
+import { useEffect, useState } from 'react';
+import { ChevronDown, ChevronUp } from 'lucide-react';
+
+export default function Technologies() {
+    const t = useTranslations('technologies');
+    const [showAll, setShowAll] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const technologies = [
+        {
+            icon: <Image src="/typescript.svg" alt="Typescript icon" width={60} height={60} />,
+            title: 'TypeScript',
+            description: t('typescript'),
+            link: 'https://www.typescriptlang.org',
+        },
+        {
+            icon: <Image src="/javascript.svg" alt="Javascript icon" width={60} height={60} className="object-contain rounded-lg" />,
+            title: 'JavaScript',
+            description: t('javascript'),
+            link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+        },
+        {
+            icon: <Image src="/react.svg" alt="React icon" width={60} height={60} />,
+            title: 'React',
+            description: t('react'),
+            link: 'https://reactjs.org',
+        },
+        {
+            icon: <Image src="/nextjs.svg" alt="Nextjs icon" width={60} height={60} />,
+            title: 'Next',
+            description: t('next'),
+            link: 'https://nextjs.org',
+        },
+        {
+            icon: <Image src="/tailwind.svg" alt="Tailwind CSS icon" width={60} height={60} />,
+            title: 'Tailwind, Css & Scss',
+            description: t('tailwind'),
+            link: 'https://tailwindcss.com/',
+        },
+        {
+            icon: <Image src="/nodejs.svg" alt="Nodejs icon" width={60} height={60} />,
+            title: 'NodeJS',
+            description: t('node'),
+            link: 'https://nodejs.org/en',
+        },
+        {
+            icon: <Image src="/java.svg" alt="Java icon" width={60} height={60} className="object-contain rounded-lg" />,
+            title: 'Java',
+            description: t('java'),
+            link: 'https://www.oracle.com/java/',
+        },
+        {
+            icon: <Image src="/python.svg" alt="Python icon" width={60} height={60} className="object-contain rounded-lg" />,
+            title: 'Python',
+            description: t('python'),
+            link: 'https://www.python.org/doc/',
+        },
+        {
+            icon: <Image src="/sql.svg" alt="SQL icon" width={60} height={60} />,
+            title: 'SQL',
+            description: t('sql'),
+            link: 'https://learn.microsoft.com/pt-br/sql/?view=sql-server-ver16',
+        },
+        {
+            icon: <Image src="/aws.svg" alt="AWS Cloud icon" width={60} height={60} />,
+            title: 'AWS Cloud',
+            description: t('aws'),
+            link: 'https://aws.amazon.com',
+        },
+        {
+            icon: <Image src="/azure.svg" alt="Azure Cloud icon" width={60} height={60} />,
+            title: 'Azure Cloud',
+            description: t('azure'),
+            link: 'https://azure.microsoft.com/pt-br/',
+        },
+        {
+            icon: <Image src="/strapi.svg" alt="Strapi icon" width={60} height={60} />,
+            title: 'Strapi',
+            description: t('strapi'),
+            link: 'https://docs.strapi.io/',
+        },
+    ];
+
+    const visibleTechnologies =
+        isMobile && !showAll ? technologies.slice(0, 5) : technologies;
+
+    return (
+        <section id="tecnologias" className="bg-white dark:bg-[var(--bg-gradient)] text-black dark:text-white pt-50 pb-20 px-0 max-w-7xl mx-auto">
+            <div className="md:text-left text-center mb-12">
+                <motion.h2
+                    className="text-4xl font-bold"
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: false, amount: 0.2 }}
+                    transition={{ duration: 0.8 }}
+                >
+                    {t('title')}
+                </motion.h2>
+                <motion.div className="mt-4 text-black dark:text-gray-400 max-w-xl">
+                    {t('description')}
+                </motion.div>
+            </div>
+
+            <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: false, amount: 0.2 }}
+                transition={{ duration: 1 }}
+                className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto"
+            >
+                {visibleTechnologies.map((tech, index) => (
+                    <TiltCard
+                        key={index}
+                        className="relative rounded-xl p-6 dark:bg-zinc-800/18 bg-gray-50 border border-gray-100 dark:border-zinc-900 cursor-pointer transition shadow-lg"
+                    >
+                        <a href={tech.link} target="_blank" rel="noopener noreferrer">
+                            <div className="flex items-center gap-5 mb-4">
+                                <div className="bg-gradient-custom rounded-lg w-14 h-14">{tech.icon}</div>
+                                <h3 className="text-xl font-bold">{tech.title}</h3>
+                            </div>
+                            <p className="text-gray-400 text-sm">{tech.description}</p>
+                        </a>
+                    </TiltCard>
+                ))}
+            </motion.div>
+            {isMobile && (
+                <div className="mt-6 text-center">
+                    <button
+                        onClick={() => setShowAll(!showAll)}
+                        className="bg-gradient-to-r from-purple-500 to-blue-500 shadow-lg hover:opacity-90 text-white px-5 py-2 rounded-lg font-semibold transition duration-200 cursor-pointer"
+                    >
+                        {showAll ? (
+                            <>
+                               {t('seeLess')}<ChevronUp className="inline-block ml-2" size={16} />
+                            </>
+                        ) : (
+                            <>
+                                {t('seeMore')} <ChevronDown className="inline-block ml-2" size={16} />
+                            </>
+                        )}
+                    </button>
+                </div>
+            )}
+        </section>
+    );
+}
